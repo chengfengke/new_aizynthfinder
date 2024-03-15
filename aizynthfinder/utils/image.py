fea0515ac17c0edc5d05209476fa6cc3e8a5dfa1
Thanafox <157661564+Thanafox@users.noreply.github.com>
2024-03-15 08:26:53 +0800

    回退条件预测

--- aizynthfinder/utils/image.py
+++ aizynthfinder/utils/image.py
@@ -16,7 +16,7 @@ from rdkit.Chem import Draw
 
 from aizynthfinder.chem import Molecule
 from aizynthfinder.utils.paths import data_path
-from aizynthfinder.utils.test_model import condition_pred
+#from aizynthfinder.utils.test_model import condition_pred
 
 if TYPE_CHECKING:
     import networkx as nx
@@ -38,7 +38,7 @@ if TYPE_CHECKING:
     )
 
 IMAGE_FOLDER = tempfile.mkdtemp()
-
+'''
 def update_condition(d):
     # 如果当前节点是叶子节点，直接返回
     if 'children' not in d:
@@ -67,7 +67,7 @@ def update_condition(d):
     sml = merged_smiles + ">>" + d['smiles']
     ans = condition_pred(sml)
     d['condition'] = ans
-
+'''
 @atexit.register
 def _clean_up_images() -> None:
     global IMAGE_FOLDER
@@ -317,7 +317,7 @@ class RouteImageFactory:
 
         self._mol_tree = self._extract_mol_tree(route)
         self._add_effective_size(self._mol_tree)
-        update_condition(self._mol_tree)
+        #update_condition(self._mol_tree)
         pos0 = (
             self._mol_tree["eff_width"] - self._mol_tree["image"].width + self.margin,
             int(self._mol_tree["eff_height"] * 0.5)
